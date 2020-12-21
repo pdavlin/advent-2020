de9e1627d3368d9c80fed9111b66140afe5bff32
pdavlin <patrick@davlin.io>
2020-12-21 13:46:15 -0600

    :sparkles: Day 3 Complete!

--- day3/main.ts
+++ day3/main.ts
@@ -4,18 +4,34 @@ const inputArray: string[] = await readInputFromFile('/home/pdavlin/development/
 
 const inputLineLen = inputArray[0].length;
 
-const parseInputLine = (line: string, lineNumber: number) => {
-    const positionToCheck = ((3 * (lineNumber - 1))) % inputLineLen
+const checkForTree = (line: string, lineNumber: number, modTarget: number) => {
+    const positionToCheck = ((modTarget * (lineNumber - 1))) % inputLineLen
     let positions = line.split('');
     return positions[positionToCheck] === '#'
 }
 
-let count = 0;
+const checks = [
+    {right: 1, down: 1},
+    {right: 3, down: 1}, // Part 1 solution
+    {right: 5, down: 1},
+    {right: 7, down: 1},
+    {right: 1, down: 2},
+]
+let countProduct: number = 1;
 
-inputArray.forEach((line, index) => {
-    if (parseInputLine(line, index + 1)) {
-        count += 1;
-    } 
+checks.forEach(checkParams => {
+    let count = 0
+        inputArray.forEach((line, index) => {
+            if (index % checkParams.down === 0) {
+                let lineToCheck = index + 1
+                if (checkForTree(line, lineToCheck, (checkParams.right / checkParams.down))) {
+                    count += 1;
+                } 
+            }
+        })
+        if (checkParams.right === 3) {
+            console.log(`Problem 1 solution: ${count}`)
+        }
+        countProduct = countProduct * count;
 })
-
-console.log(count)
+console.log(`Problem 2 solution: ${countProduct}`)
